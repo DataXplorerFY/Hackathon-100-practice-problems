3f09679bf463faeec64d2af60b80ff62a9967ca9
DataXplorerFY <farhanyounas5204@gmail.com>
2023-10-27 22:54:58 +0500

    question 22 added

--- Hackathone 1 Practice problems/Hackathone_.ts
+++ Hackathone 1 Practice problems/Hackathone_.ts
@@ -414,3 +414,20 @@ const elementToRemove: number = 3;
 
 removeElementFromArray(iinputArray, elementToRemove);
 console.log(iinputArray); 
+
+//22. Check if two arrays are equal (have the same elements in the same order).
+// - Input: [1, 2, 3], [1, 2, 3]
+// - Output: true
+function areArraysEqual(array1: any[], array2: any[]): boolean {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+}
